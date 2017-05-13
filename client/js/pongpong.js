324be27723e581fd36870f64f81b06db35d09452
thefeeltrain <braydeneriq@gmail.com>
2017-05-12 21:44:55 -0700

    Basic server connection

--- client/js/pongpong.js
+++ client/js/pongpong.js
@@ -1,20 +1,71 @@
+var server,
 
-//Audio handler
-let audio = {
-    synth: new Audio("audio/synth.mp3"),
-    hover: new Audio("audio/ui-hover.wav"),
-    click: new Audio("audio/ui-click.wav")
-};
+    //Audio handler, keeps audio objects together
+    audio = {
+        synth: new Audio("audio/synth.mp3"),
+        hover: new Audio("audio/ui-hover.wav"),
+        click: new Audio("audio/ui-click.wav")
+    },
 
-let Game = {
-    settings: {
-        maxplayers: 4,
-        visibility: 1,
-        linusmode: 0
-    }
-};
+    //Game object
+    Game = {
+        //Settings related to the game, can be changed on the "Create Game" screen
+        settings: {
+            maxplayers: 4,
+            visibility: 1,
+            linusmode: 0
+        }
+    },
+
+    //Player object
+    Player = {
+        "name": localStorage.getItem('username') || "New001"
+    },
+
+    //Update at glorious 60fps
+    GameLoop = setInterval(loop,1000/60);
 
 function initialize() {
+
+    //Connect to server, temp use localhost
+    server = new WebSocket("ws://localhost:8082");
+
+    //Let user know if socket cannot connect
+    server.onerror = function() {
+        alert("Unable to connect to server.");
+    }
+
+    //Initialize the connection to the server
+    server.onopen = function (event) {
+        let msg = {
+            type: "connect",
+            name: Player.name
+        };
+        server.send(JSON.stringify(msg));
+    };
+
+    //Recieve data from server
+    server.onmessage = function (event) {
+
+        //Parse as JSON
+        let msg = JSON.parse(event.data);
+
+        //Initialize player with generated ID
+        if(msg.type == "init") {
+            Player.ID = msg.id;
+        }
+
+        //Add player to their own lobby
+        else if(msg.type == "lobbyCreated") {
+            Player.lobby = Player.ID;
+            console.log("Lobby created successfully.");
+        }
+
+        else if(msg.type == "sync") {
+            console.log(msg);
+        }
+
+    }
 
     // Main music loop, volume is temp while settings are made
     audio.synth.loop = 1;
@@ -23,28 +74,41 @@ function initialize() {
 
     // Bind UI hover sound effect to hover
     audio.hover.volume = 0.1;
-    $('.main-menu > ul > li').mouseover(function() {
+    $('.main-menu > ul > li, .setting > ul > li, .start, .back').mouseover(function() {
+
+        //Restart and play sound effect
         audio.hover.currentTime = 0;
         audio.hover.play();
+
     });
 
-    // Bind UI click sound effect to hover
+    // Bind UI click sound effect to click
     audio.click.volume = 0.1;
     $('.setting > ul > li').click(function() {
+
+        //Restart and play sound effect
         audio.click.currentTime = 0;
         audio.click.play();
+
     });
 
     // Bind main menu view swaps
-    $('li[data-view]').click(function() {
+    $('*[data-view]').click(function() {
+
+        //Get view to change to
         let to = $(this).attr('data-view');
+
         // Hide current view, show new view
-        $('.showing').fadeOut(400);
-        $('.'+to).fadeIn(400);
+        $('.showing').removeClass('showing');
+        $('.'+to).addClass('showing');
+
     });
 
+    // Create lobby on click
+    $('*[data-action="start-lobby"]').click(createLobby);
+
     // Bind quit to click event
-    $('li[data-view="quit"]').click(quit);
+    $('*[data-action="quit"]').click(quit);
 
     //Game creation settings
     $('.setting > ul > li').click(function() {
@@ -64,16 +128,47 @@ function initialize() {
 
 }
 
+function loop() {
+
+    //Sync game with server when in a lobby
+    if(Player.lobby) {
+
+        let msg = {
+            //Uses the type "sync" to ask for updates
+            type: "sync",
+            id: Player.ID,
+            lobby: Player.lobby
+        };
+
+        //Send data as string instead of JSON
+        server.send(JSON.stringify(msg));
+
+    }
+
+}
+
+function createLobby() {
+
+    //Let the server know this ID wants to create a lobby
+    let msg = {
+        type: "createLobby",
+        id: Player.ID,
+        settings: Game.settings
+    };
+
+    server.send(JSON.stringify(msg));
+
+}
+
 function quit() {
 
-    // Make sure the user actually wants to leave
-    let yes = confirm("Are you sure you want to quit PongPong?");
-    if(yes) {
-        //Does not actually work in-browser, purely theoretical
-        window.close();
-    }
+    //Close connection to server
+    server.close();
 
 }
 
 // Runs initialize when the page loads
 window.onload = initialize;
+
+//Runs when the page is unloaded
+window.onunload = quit;
---
+++ server/pingping.js
@@ -0,0 +1,133 @@
+//Helper function to clear console
+console.reset = function () {
+    return process.stdout.write('\033c');
+}
+
+//Initialize players and lobbies
+var players = {},
+    lobbies = {},
+    IDs = [],
+    //Loop console output every second
+    looping = setInterval(loop,1000);
+
+//Create websocket
+var ws = require("nodejs-websocket"),
+    server = ws.createServer(function(conn) {
+
+        //Socket handler
+        conn.on("text", function(str) {
+
+            //Parse data as JSON
+            let data = JSON.parse(str);
+
+            //Event when a player connects
+            if(data.type == "connect") {
+                var msg = {
+                    "type": "init",
+                    //Give each player a unique identifier
+                    "id": generateUID()
+                }
+
+                //Connect name to ID and create the player object
+                players[msg.id] = {
+                    name: data.name
+                };
+
+                //Remove player and their lobby if they disconnect
+                conn.on("close", function() {
+                    delete players[msg.id];
+                    delete lobbies[msg.id];
+                });
+
+            }
+
+            //Main client update method
+            else if(data.type == "sync") {
+
+                //Make sure the player exists and is in a lobby
+                if(IDs.includes(data.id) && data.lobby != null) {
+
+                    var msg = {
+                        "type": "sync",
+                        "lobby": lobbies[data.lobby]
+                    }
+
+                }
+
+                //Let the player know that their ID is invalid
+                else {
+                    var msg = {
+                        "type": "error",
+                        "text": "Your ID is invalid."
+                    }
+                }
+
+            }
+
+            //When the player wants to create a lobby
+            else if(data.type == "createLobby") {
+
+                //Make sure the player exists
+                if(IDs.includes(data.id)) {
+
+                    //Add new lobby to the list
+                    lobbies[data.id] = data.settings;
+
+                    //Set current player count to 1
+                    lobbies[data.id].current = 1;
+
+                    //Let the player know the lobby was created successfully
+                    var msg = {
+                        "type": "lobbyCreated"
+                    }
+
+                }
+
+                //Let the player know that their ID is invalid
+                else {
+                    var msg = {
+                        "type": "error",
+                        "text": "Your ID is invalid."
+                    }
+                }
+            }
+
+            //Send the generated message if it exists
+            if(msg) {
+                conn.sendText(JSON.stringify(msg));
+            }
+
+        })
+
+    }).listen(8082);
+
+function loop() {
+    //Reset console and output various metrics
+    console.reset();
+    console.log("PongPong Server\n");
+    console.log("# of Players: " + Object.keys(players).length);
+    console.log("List of IDs: " + IDs);
+    console.log("# of Lobbies: " + Object.keys(lobbies).length + "\n");
+    console.log("List of Lobbies:");
+    console.log("Host\t\tPlayers\t\tPublic");
+    for(var i=0; i < Object.keys(lobbies).length; i++) {
+        var e = Object.keys(lobbies)[i];
+        console.log(e + "\t\t"+lobbies[e].current+"/" + lobbies[e].maxplayers + "\t\t" + lobbies[e].visibility);
+    }
+}
+
+function generateUID(length = 4) {
+
+    //Generate random unique indentifier
+    let uid = Math.random().toString(36).substring(2,length+2).toUpperCase();
+
+    //Make sure it is unique, regenerate if not
+    while(IDs.includes(uid)) {
+        uid = Math.random().toString(36).substring(2,length+2).toUpperCase();
+    }
+
+    //Keep a list of all IDs
+    IDs.push(uid);
+
+    return uid;
+}
